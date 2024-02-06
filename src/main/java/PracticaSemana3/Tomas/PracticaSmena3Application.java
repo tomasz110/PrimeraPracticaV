8630cbbabbc74e6cdbb8b60f80ebe1374aae96b3
tomas <tomas@TomasLaptop>
2024-02-06 10:45:52 -0600

    Primera Practica

---
+++ src/main/java/PracticaSemana3/Tomas/PracticaSmena3Application.java
@@ -0,0 +1,13 @@
+package PracticaSemana3.Tomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSmena3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSmena3Application.class, args);
+	}
+
+}
---
+++ src/test/java/PracticaSemana3/Tomas/PracticaSmena3ApplicationTests.java
@@ -0,0 +1,13 @@
+package PracticaSemana3.Tomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSmena3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
